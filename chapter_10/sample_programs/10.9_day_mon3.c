9dadab6c864593e3264c5139f9f4cd9f392263ad
Elisey <enovikow6@gmail.com>
2021-01-18 04:50:55 +0300

    Chapter 10 programming samples

---
+++ chapter_10/sample_programs/10.9_day_mon3.c
@@ -0,0 +1,16 @@
+/* day_mon3.c -- uses pointer notation */
+
+#include <stdio.h>
+
+#define MONTHS 12
+
+int main(void) {
+    int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int index;
+
+    for (index = 0; index < MONTHS; index++) {
+        printf("Month %2d has %2d days.\n", index + 1, *(days + index));
+    }
+
+    return 0;
+}
