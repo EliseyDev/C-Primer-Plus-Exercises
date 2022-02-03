83064668b2915ebdfc0b95d78caed1a32a9a9fd6
Elisey <enovikow6@gmail.com>
2022-02-03 18:15:28 +0300

    Chapter 11 programming exercises 11.7 refactor.

--- chapter_11/programming_exercises/11.7.c
+++ chapter_11/programming_exercises/11.7.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-char *my_strncpy(char *s1, const char *s2, int n);
+char *my_strncpy(const char *src, char *dest, int n);
 
 int main(void) {
-//    char *s1 = "str1";
-//    char *s2 = "string2";
-    char s1[5];
-    char *s2 = "";
-    my_strncpy(s1, s2, 4);
+    int copy_times;
+    int str_length;
 
-    printf("%s\n", s1);
-    printf("%s\n", s2);
+    printf("Enter copy cycles and string length: ");
+    scanf("%d %d\n", &copy_times, &str_length);
+
+    char dest[str_length];
+    char src[str_length];
+    for (int i = 0; i < copy_times; i++) {
+        scanf("%s", src);
+        my_strncpy(src, dest, 4);
+        printf("%s\n", src);
+    }
 }
 
-char *my_strncpy(char *const s1, const char *const s2, int n) {
-    if (s1 != NULL && s2 != NULL) {
+char *my_strncpy(const char *const src, char *const dest, int n) {
+    if (src != NULL && dest != NULL) {
         for (int i = 0; i < n; i++) {
-            if (s2[i] == '\0') {
-                s1[i] = '\0';
+            if (src[i] == '\0') {
+                dest[i] = '\0';
             } else {
-                s1[i] = s2[i];
+                dest[i] = dest[i];
             }
         }
     }
 
-    return s1;
+    return dest;
 }
 
